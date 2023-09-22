4b55d4fa1bdbe38f00a6f1bfadc3e21b28fd2c30
rubenkretek <rubenkretek@outlook.com>
2023-09-22 15:37:01 +0100

    Couple basic challenges

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,23 @@
-function calculator() {}
+function calculator(number1, number2, operator) {
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = number1 + number2;
+      break;
+    case "-":
+      result = number1 - number2;
+      break;
+    case "*":
+      result = number1 * number2;
+      break;
+    case "/":
+      result = number1 / number2;
+      break;
+    default:
+      throw new Error("Invalid operator!");
+  }
+  return result;
+}
 
 module.exports = calculator;
--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,14 @@
-function countOccurrences() {}
+function countOccurrences(word, letter) {
+  let letterOccurance = 0;
+  const lowerCaseWord = word.toLowerCase();
+  const lowerCaseLetter = letter.toLowerCase();
+
+  Array.from(lowerCaseWord).forEach((element) => {
+    if (element === lowerCaseLetter) {
+      letterOccurance++;
+    }
+  });
+  return letterOccurance;
+}
 
 module.exports = countOccurrences;
