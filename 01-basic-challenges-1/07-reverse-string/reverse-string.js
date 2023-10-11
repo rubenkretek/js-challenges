b323ecbafd02455d905448de167d628d96ac3040
rubenkretek <rubenkretek@outlook.com>
2023-10-11 16:00:59 +0100

    Reversed string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,15 @@
-function reverseString() {}
+function reverseString(str) {
+  const strArr = str.split("");
+  let reverseArr = [];
+  let reverseStr;
+
+  strArr.forEach((letter) => {
+    reverseArr.unshift(letter);
+  });
+
+  reverseStr = reverseArr.join("");
+
+  return reverseStr;
+}
 
 module.exports = reverseString;
